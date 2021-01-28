718fb761c71b4b49969516f5c31ca053756a3c24
Relyxes <72753205+Relyxes@users.noreply.github.com>
2021-01-28 23:01:43 +0500

    Add files via upload

---
+++ MyList.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project_MyList
+{
+    public class MyList
+    {
+        private int[] _tempItems;
+        private int[] _items;
+
+        public MyList()
+        {
+            _items = new int[] { };
+        }
+
+        public MyList(int range)
+        {
+            if (range < 0) Console.WriteLine("Индекс не может быть меньше 0.");
+            else _items = new int[range];
+        }
+
+        public int Count()
+        {
+            return _items.Length;
+        }
+
+        public void Add(int item)
+        {
+            Resize(ref _items, _items.Length + 1);
+            _items[_items.Length - 1] = item;
+        }
+
+        public void ShowMyList()
+        {
+            foreach (int item in _items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public bool Remove(int item)
+        {
+            int index = IndexOf(item);
+            if (index < 0) return false;
+            RemoveAt(index);
+            return true;
+        }
+
+        private void RemoveAt(int index)
+        {
+            _tempItems = new int[_items.Length - 1 - index];
+            for (int i = index + 1; i < _items.Length; i++)
+            {
+                _tempItems[i - index - 1] = _items[i];
+            }
+
+            for (int i = index; i < _items.Length - 1; i++)
+            {
+                _items[i] = _tempItems[i - index];
+            }
+
+            Resize(ref _items, _items.Length -1);
+        }
+
+        public int IndexOf(int item)
+        {
+            return Array.IndexOf(_items, item);
+        }
+
+
+        public void Clear()
+        {
+            Array.Clear(_items, 0, _items.Length);
+        }
+
+        private void Resize(ref int[] _items, int newSize)
+        {
+            _tempItems = new int[newSize];
+
+            if (newSize > _items.Length)
+            {
+                for (int i = 0; i < newSize-1; i++)
+                {
+                    _tempItems[i] = _items[i];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < newSize; i++)
+                {
+                    _tempItems[i] = _items[i];
+                }
+            }
+
+            _items = _tempItems;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project_MyList
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //Создаем собственную коллекцию начальной емкостью в 2 элемента.
+            MyList testList = new MyList(2);
+            //Создаем собственную пустую коллекцию.
+            MyList newMyList = new MyList();
+
+            // Добавляем 2 элемента.
+            testList.Add(123);
+            testList.Add(345);
+
+            // Добавляем 2 элемента.
+            newMyList.Add(456);
+            newMyList.Add(789);
+
+            // Получаем количество элементов коллекции.
+            Console.WriteLine("Количество элементов коллекции testList: {0}",testList.Count());
+            Console.WriteLine("Количество элементов коллекции newMyList: {0}",newMyList.Count());
+
+            //Выводим список элементов коллеции.
+            Console.WriteLine("\nСписок элементов коллекции testList: ");
+            testList.ShowMyList();
+            Console.WriteLine("Список элементов коллекции newMyList: ");
+            newMyList.ShowMyList();
+
+            // Получаем индекс элемента из коллекции по имени.
+            int index = testList.IndexOf(123);
+            Console.WriteLine("\nИндекс элемента коллекции testList, по имени - {0}: {1}",123,index);
+
+            // Удаляем элемент из коллекции по имени.
+            testList.Remove(123);
+            newMyList.Remove(789);
+
+            Console.WriteLine("\nСписок элементов коллекции testList после удаления: ");
+            testList.ShowMyList();
+            Console.WriteLine("Список элементов коллекции newMyList после удаления: ");
+            newMyList.ShowMyList();
+
+            // Очищаем элементы коллекции.
+            testList.Clear();
+            Console.WriteLine("\nСписок элементов коллекции testList после очищения: ");
+            testList.ShowMyList();
+
+            Console.ReadLine();
+
+        }
+    }
+}
